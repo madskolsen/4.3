a6f7705c8accd4db972f9a6aa381f773ad206175
madskolsen <89842092+madskolsen@users.noreply.github.com>
2021-09-08 12:12:45 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void zoop() {                             //4
+        baffle();                                           //5
+        System.out.print("You wugga ");                     //11
+        baffle();                                           //12
+    }
+
+    public static void baffle() {                           //6     //13      //20
+        System.out.print("wug");                            //7     //14      //21
+        ping();                                             //8     //15      //22
+    }
+
+    public static void ping() {                             //9     //16      //23
+        System.out.println(".");                            //10    //17      //24
+    }
+
+
+    public static void main(String[] args) {                //1
+        System.out.print("No, I ");                         //2
+        zoop();                                             //3
+        System.out.print("I ");                             //18
+        baffle();                                           //19
+
+        //No, I wug.
+        //You wugga wug.
+        //I wug.
+    }
+}
